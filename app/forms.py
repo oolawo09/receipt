7e4398f66eb15e1e97ffbdb4c83c9efc42474b4a
oolawo09 <aolawo@alueducation.com>
2020-01-23 22:59:09 +0300

    added form fields

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,27 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import Form
+from wtforms import IntegerField, StringField, FormField, DateField
 from wtforms.validators import DataRequired
 
+
+class TelephoneForm(Form):
+    country_code = IntegerField('Country Code', validators=[DataRequired()])
+    area_code = IntegerField('Area Code/Exchange', validators=[DataRequired()])
+    number = StringField('Number')
+
+
+class ContactDetailsForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    email = StringField('Email', )
+    phone = FormField(TelephoneForm)
+
+
+class OrderDetailsForm(FlaskForm):
+    number = IntegerField('Number', validators=[DataRequired])
+    date = DateField('Date', validators=[DataRequired])
+
 class ReceiptForm(FlaskForm):
-    submit = SubmitField('Download')
-      
+    sender_details = FormField(ContactDetailsForm)
+    recipient_details = FormField(ContactDetailsForm)
+    order_details = FormField(OrderDetailsForm)
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from flask import render_template
 from app import app
+from app.forms import ContactDetailsForm, OrderDetailsForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
-
-
+    sender_form = ContactDetailsForm() 
+    recipient_form = ContactDetailsForm()
+    order_details_form = OrderDetailsForm()
+    return render_template('receipt.html', sender_form=sender_form, recipient_form=recipient_form, order_details_form=order_details_form)
