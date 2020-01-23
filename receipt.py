742448fc488871397f4c80e855c5c70be3871066
oolawo09 <aolawo@alueducation.com>
2020-01-23 21:17:50 +0300

    refactor

--- receipt.py
+++ receipt.py
@@ -1,11 +1,1 @@
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return 'hello CI/CD world'
-
-
-if __name__ == '__main__':
-    app.run()
+from app import app
