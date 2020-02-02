0e0b7bc981e3debcc98894205d16129c75f41718
oolawo09 <aolawo@alueducation.com>
2020-02-02 14:39:57 +0300

    done with basic form. cut out a lot

--- app/forms.py
+++ app/forms.py
@@ -1,36 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import Form
 from wtforms import IntegerField, StringField, \
     FormField, DateField, TextAreaField
 from wtforms.validators import DataRequired
 
 
-class TelephoneForm(Form):
-    country_code = IntegerField('Country Code', validators=[DataRequired()])
-    area_code = IntegerField('Area Code/Exchange', validators=[DataRequired()])
-    number = StringField('Number')
-
-
-class ContactDetailsForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    address = StringField('Address', validators=[DataRequired()])
-    email = StringField('Email', )
-    phone = FormField(TelephoneForm)
-
-
-class OrderDetailsForm(FlaskForm):
-    number = IntegerField('Number', validators=[DataRequired()])
-
-
 class LineItemForm(FlaskForm):
     description = StringField('Description', validators=[DataRequired()])
-    rate = IntegerField('Rate', validators=[DataRequired()])
+    price = IntegerField('Price', validators=[DataRequired()])
     quantity = IntegerField('Quantity', validators=[DataRequired()])
-    amount = IntegerField('Amount', validators=[DataRequired()])
-    additional_details = TextAreaField('Additional details')
 
 
 class ReceiptForm(FlaskForm):
-    sender_details = FormField(ContactDetailsForm)
-    recipient_details = FormField(ContactDetailsForm)
-    order_details = FormField(OrderDetailsForm)
+    sender = StringField('Sender', validators=[DataRequired()])
+    recipient = StringField('Recipient', validators=[DataRequired()])
+    items = FormField(LineItemForm)
+    notes = TextAreaField('Notes')
--- app/routes.py
+++ app/routes.py
@@ -1,43 +1,19 @@
 from flask import url_for, render_template, redirect
 from app import app
-from app.forms import ContactDetailsForm, OrderDetailsForm
+from app.forms import ReceiptForm
 
 
-@app.route('/')
-@app.route('/index', )
+@app.route('/', methods=['get', 'post'])
+@app.route('/index', methods=['get', 'post'])
+@app.route('/create_receipt', methods=['get', 'post'])
 def index():
-    return render_template('base.html')
-
-
-@app.route('/sender_contact_form', methods=['get', 'post'])
-def show_sender_contact_form():
-    form = ContactDetailsForm()
+    form = ReceiptForm()
     if form.validate_on_submit():
-        return redirect(url_for('show_recipient_contact_form'), code=200)
-    return render_template('sender_contact_details.html',
-                           contact_form=form,
-                           user="sender")
+        return redirect(url_for('preview'), code=200)
+    return render_template('receipt.html',
+                           form=form)
 
 
-@app.route('/recipient_contact_form', methods=['get', 'post'])
-def show_recipient_contact_form():
-    form = ContactDetailsForm()
-    if form.validate_on_submit():
-        return redirect(url_for('show_receipt_details'), code=200)
-    return render_template('recipient_contact_details.html',
-                           contact_form=form,
-                           user="recipient")
-
-
-@app.route('/show_order_details_form', methods=['get', 'post'])
-def show_order_details_form():
-    form = OrderDetailsForm()
-    if form.validate_on_submit():
-        return redirect(url_for('show_preview'), code=200)
-    return render_template('order_details.html',
-                           order_form=form)
-
-
-@app.route('/show_preview', methods=['get'])
-def show_preview():
-    return 'preview placeholder'
+@app.route('/preview', methods=['get', 'post'])
+def preview():
+    return render_template('preview.html')
